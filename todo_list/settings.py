688636fd21cec332d6c08755c229e6e87a401b52
unknown <abdulbasitadio@gmail.com>
2022-08-19 13:38:50 +0100

    deploying

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -53,10 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages', 
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'base',
     'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
